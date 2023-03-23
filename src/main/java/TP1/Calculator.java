7a984caf42a35716e1a5f1ba450f06fc891bbf2c
cataghiglione <catalina.ghiglione@ing.austral.edu.ar>
2023-03-23 14:52:44 -0300

    hola

---
+++ src/main/java/TP1/Calculator.java
@@ -0,0 +1,33 @@
+package TP1;
+
+public class Calculator implements Calculator_I {
+    @Override
+    public String sum(String a, String b) {
+
+    }
+
+    @Override
+    public String sub(String a, String b) {
+        return null;
+    }
+
+    @Override
+    public String mult(String a, String b) {
+        return null;
+    }
+
+    @Override
+    public String div(String a, String b) {
+        return null;
+    }
+
+    @Override
+    public String toHex(String binary) {
+        return null;
+    }
+
+    @Override
+    public String fromHex(String hex) {
+        return null;
+    }
+}
---
+++ src/main/java/TP1/Calculator_I.java
@@ -0,0 +1,10 @@
+package TP1;
+
+public interface Calculator_I {
+    String sum(String a, String b);
+    String sub(String a, String b);
+    String mult(String a, String b);
+    String div(String a, String b);
+    String toHex(String binary);
+    String fromHex(String hex);
+}
